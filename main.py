5d4e21a5f6fd5b40fb7d1022a1c2efad8f3cb81f
Kilian Keneder <ge35wor@mytum.de>
2023-11-13 11:02:10 +0100

    split by game, not yet tested

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ with open("configuration.json", "r") as config:
     data = json.load(config)
     token = data["token"]
     MATCHES_AND_RESULTS = data["matches_and_results_channel"]
+    result_channel = data["result_channel"]
 
 locale.setlocale(locale.LC_ALL, "de_DE")
 
@@ -65,6 +66,7 @@ async def add_result(
 
     uhrzeit.strip()
     datum.strip()  # es schleichen sich schnell führende und endende leerzeichen in userinput -> strip removed diese
+    channel_id = interaction.channel_id
 
     try:
         date_format = result_list.zeit_format(datum=datum, zeit=uhrzeit)  # format
@@ -87,7 +89,8 @@ async def add_result(
         "ergebnis": ergebnis.strip(),
         "format": format.strip(),
         "message_id": message_id,
-        "datetime": date_format
+        "datetime": date_format,
+        "game": result_channel[channel_id]
     }
 
     try:
@@ -257,15 +260,30 @@ async def update_kw_message(jahr_KW, liste):
         await delete_kw_message(jahr_KW, message)
         return
 
-    new_text = f"# __KW {jahr_KW}:__\n\n"  # # ist für Header        TODO kann man hier nach Spiel sortieren?
-    for result in liste:
-        new_text += (
-            result_list.result_to_string(result) + "\n"
-        ) 
-
-    await message.edit(content=new_text)
+    text = kw_string(liste, jahr_KW)
+    await message.edit(content = text)
     print(f'finished main.update_KW_Message for KW: {jahr_KW}\n')
 
+def kw_string(liste, jahr_KW):
+
+    split_dict = dict()
+    while liste:
+        current_game = liste[0]["game"] #take first results game
+        split_dict[current_game] = list()   #create a list for each game
+        for i in liste:
+            if i["game"] == current_game:
+                # fülle neue liste mit allen results die zu current game gehören
+                split_dict[current_game].append(liste[i])   
+            #sollte automatisch nach zeit sortiert sein weil input liste nach zeit sortiert war
+
+    new_text = f"# __KW {jahr_KW}:__\n\n"  # # ist für Header
+    for split_liste in split_dict:
+        game = split_liste[0]["game"]
+        new_text += "## " + game + "\n"
+        for result in split_liste:
+            new_text += (
+                result_list.result_to_string(result) + "\n"
+            ) 
 
 async def delete_kw_message(jahr_KW, message):
     dict = result_list.read("dictionary")
